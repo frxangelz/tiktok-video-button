fe46f4023ecab191dfd08bff76c419b844d6a006
Aulia Wini <frxangelz@gmail.com>
2021-08-16 22:13:58 +0700

    update inject.js
    tiktok layout changed

--- inject.js
+++ inject.js
@@ -6,6 +6,7 @@
 */
 function CreateBtn(a,src){
 
+	//console.log("create video button");
 	var btn = document.createElement('div');
 	btn.innerHTML = "<br /><a href='"+src+"' target='_blank' class='follow-button tiktok-btn-pc-line'>&nbsp; Video &nbsp;</a>";
 	a.parentNode.insertBefore(btn,a.nextSibling);
@@ -22,17 +23,22 @@ function InjectButton(){
 	var vid;
 	var src;
 	
+	//console.log("got video : "+vids.length);
+	
 	for(var i=0; i<vids.length; i++){
 		
 		dataId = vids[i].getAttribute("data-id");
 		if(dataId == "true") { continue; }
 		
-		vid = vids[i].getElementsByTagName('video');
-		if((!vid) || (vid.length < 1)) { continue; }
+		vid = vids[i].parentElement.parentElement.getElementsByTagName('video');
+		if((!vid) || (vid.length < 1)) { 
+			console.log("No video found, next items");
+			continue; 
+		}
 		src = vid[0].getAttribute('src');
 		if(!src) { continue; }
 
-		CreateBtn(vids[i],src);
+		CreateBtn(vids[i].parentElement.parentElement,src);
 		vids[i].setAttribute("data-id","true");
 	}
 }
