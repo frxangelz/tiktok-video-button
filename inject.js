de4cf74d0eb5caccdd6d95913f96973b26401cdd
aulia <frxangelz@users.noreply.github.com>
2021-11-03 14:54:00 +0700

    fix element changed

--- inject.js
+++ inject.js
@@ -6,15 +6,22 @@
 */
 function CreateBtn(a,src){
 
-	//console.log("create video button");
+	var dataID = a.getAttribute("data-id");
+	if(dataID === "1") { 
+		return; 
+	}
+	
+	a.setAttribute("data-id","1");
+	
 	var btn = document.createElement('div');
-	btn.innerHTML = "<br /><a href='"+src+"' target='_blank' class='follow-button tiktok-btn-pc-line'>&nbsp; Video &nbsp;</a>";
+	//btn.innerHTML = "<br /><a href='"+src+"' target='_blank' class='e1v1ai9g0 tiktok-jcprrh-Button-StyledFollowButton ehk74z00'>&nbsp; Video &nbsp;</a>";
+	btn.innerHTML = "<br /><a href='"+src+"' target='_blank' class=''>&nbsp; <button>Video</button> &nbsp;</a>";
 	a.parentNode.insertBefore(btn,a.nextSibling);
 }
 
 function InjectButton(){
 	
-	var vids = document.querySelectorAll('a.item-video-card-wrapper');
+	var vids = document.getElementsByTagName("video");
 	if((!vids) || (vids.length < 1)) { 
 		console.log("No Video found");
 		return; 
@@ -22,24 +29,24 @@ function InjectButton(){
 	
 	var vid;
 	var src;
-	
-	//console.log("got video : "+vids.length);
+	var dataId = 0;
 	
 	for(var i=0; i<vids.length; i++){
 		
 		dataId = vids[i].getAttribute("data-id");
-		if(dataId == "true") { continue; }
-		
-		vid = vids[i].parentElement.parentElement.getElementsByTagName('video');
-		if((!vid) || (vid.length < 1)) { 
-			console.log("No video found, next items");
+		if(dataId === "1") { 
 			continue; 
 		}
-		src = vid[0].getAttribute('src');
+		
+		vids[i].setAttribute("data-id","1");
+		src = vids[i].getAttribute('src');
 		if(!src) { continue; }
+	
+		if(src.indexOf("tiktokcdn.com") === -1) { 
+			continue; 
+		}
 
-		CreateBtn(vids[i].parentElement.parentElement,src);
-		vids[i].setAttribute("data-id","true");
+		CreateBtn(vids[i].parentElement.parentElement.parentElement.parentElement.parentElement,src);
 	}
 }
 
